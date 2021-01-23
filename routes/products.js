75924eca33c037324e3d93c73d3118c23636bef9
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-23 15:32:21 -0700

    created basic endpoints tested them with postman

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,29 @@
 const express = require("express")
 const router = express.Router()
 
+// Get all products
+router.get("/", (req, res) => {
+    res.send("got all products")
+})
+
+// Get one product 
+router.get("/:id", (req, res) => {
+    res.send("got one product by id")
+})
+
+// Add new product
+router.post("/", (req, res) => {
+    res.send('hit the post route')
+})
+
+// Patch product
+router.patch("/:id", (req, res) => {
+    res.send("hit the patch route")
+})
+
+// Delete product 
+router.delete("/:id", (req, res) => {
+    res.send("hit delete route")
+})
+
 module.exports = router
