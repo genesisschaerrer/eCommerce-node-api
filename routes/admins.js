ce1a4cbd919adc89f347594dd0066b66b1019955
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-24 16:50:39 -0700

    finally got the authentication post route to work

--- routes/admins.js
+++ routes/admins.js
@@ -1,21 +1,26 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
 
 const Admins = require("../models/admin-model")
-const { route } = require("./products")
+// const { route } = require("./products")
+
 
 //Create new admin
 router.post("/adminschaerrer/register", async(req, res)=> {
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
     const admin = new Admins({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     })
     try {
         const newAdmin = await admin.save()
         res.status(200).json(newAdmin)
     } catch (error) {
-        res.status(400).json({messege: error.messege})
+        res.status(400).json({messege: error})
     }
 })
 
@@ -28,6 +33,22 @@ router.delete("/adminschaerrer/delete/:id", async(req, res) => {
             res.status(200).json({messege: `${admin.username} has been deleted as an admin`})
         }
     })
+})
+
+//Login
+router.post("/adminlogin", async (req, res) => {
+        const admin = await Admins.findOne({username: req.body.username, email: req.body.email})
+        if(!admin){
+           return res.status(400).json({messege: "username or email not found"})
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, admin.password)
+        if(!validPassword){
+            return res.status(400).send.json({messege: "invalid password"})
+        } 
+    
+        return res.status(200).json({messege: "Logged In"})
+    
 })
 
 
