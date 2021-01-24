745efcf17b53786ac7757b4db80a75db62d08b79
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-24 12:56:40 -0700

    working post and delete routes for admin

--- routes/admins.js
+++ routes/admins.js
@@ -2,7 +2,9 @@ const express = require("express")
 const router = express.Router()
 
 const Admins = require("../models/admin-model")
+const { route } = require("./products")
 
+//Create new admin
 router.post("/adminschaerrer/register", async(req, res)=> {
     const admin = new Admins({
         username: req.body.username,
@@ -15,6 +17,17 @@ router.post("/adminschaerrer/register", async(req, res)=> {
     } catch (error) {
         res.status(400).json({messege: error.messege})
     }
+})
+
+//Delete admin
+router.delete("/adminschaerrer/delete/:id", async(req, res) => {
+    Admins.findByIdAndDelete(req.params.id, (error, admin)=> {
+        if(error){
+            res.status(404).json({messege: error.messege})
+        } else {
+            res.status(200).json({messege: `${admin.username} has been deleted as an admin`})
+        }
+    })
 })
 
 
