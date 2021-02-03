16c8ee4b9b3502cc210e666c700c9ba93e144d4c
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-02-03 14:17:54 -0700

    testing stripe

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+// const uuid = require("uuid/v4")
+const { v4: uuidv4 } = require('uuid')
+
 const Products = require("../models/product-model")
 const verify = require("../verify-token")
 
@@ -99,6 +103,36 @@ router.delete("/product/:id", verify, getProduct, async (req, res) => {
         res.json({message: `${res.product.name} has been deleted from database`})
     } catch (error) {
         res.status(500).json({message: error.message})
+    }
+})
+
+//Stripe
+router.post("/payment", async (req, res) => {
+    try{
+        const {purchaseAmount, token} = req.body
+        console.log("Purchase Amount: ", purchaseAmount)
+        const idempontencyKey = uuidv4()
+
+        return stripe.customers.create({
+            email: token.email,
+            source: token.id
+        }).then(custumer => {
+            stripe.charges.create({
+                amount: purchaseAmount * 100,
+                currency: "usd",
+                custumer: custumer.id,
+                receipt_email: token.email,
+                shipping: {
+                    name: token.card.name,
+                    address: {
+                        country: token.card.address_country
+                    }
+                }
+            }, {idempontencyKey})
+        }).then(result => res.status(200).json(result))
+        
+    } catch (error){
+        res.status(400).json({message: error.message})
     }
 })
 
